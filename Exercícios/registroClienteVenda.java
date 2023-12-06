fa246a0cac52e9b8f9349c4553d71e1440a599a8
Maria Eduarda Teodoro <madsfts@gmail.com>
2023-12-06 15:20:17 -0300

    Update registroClienteVenda.java

--- Exercícios/registroClienteVenda.java
+++ Exercícios/registroClienteVenda.java
@@ -7,53 +7,34 @@ public class registroClienteVenda {
     static final String senha = "";
 
     public static void main(String[] args) {
-
         try (Connection conexao = DriverManager.getConnection("jdbc:mysql://" + host + ":3306/" + database, usuario, senha)) {
+            // Inserir dados na tabela clientes
+            String sqlInserirClientes = "INSERT INTO clientes (id_cliente, nome, email) VALUES " +
+                                        "(1, 'Ana', 'ana@example.com'), " +
+                                        "(2, 'Pedro', 'pedro@example.com')";
 
-            // Inserting data into 'clientes' table
-            String nomeCliente = "Juninho";
-            String emailCliente = "cliente@email.com";
-
-            String sqlCliente = "INSERT INTO clientes (nome, email) VALUES (?, ?)";
-            try (PreparedStatement stmtCliente = conexao.prepareStatement(sqlCliente, Statement.RETURN_GENERATED_KEYS)) {
-                stmtCliente.setString(1, nomeCliente);
-                stmtCliente.setString(2, emailCliente);
-
-                int linhasAfetadasCliente = stmtCliente.executeUpdate();
-
-                if (linhasAfetadasCliente > 0) {
-                    System.out.println("Cliente registrado com sucesso!");
-
-                    // Retrieving the generated id_cliente
-                    ResultSet generatedKeys = stmtCliente.getGeneratedKeys();
-                    int idCliente = 0;
-                    if (generatedKeys.next()) {
-                        idCliente = generatedKeys.getInt(1);
-                    }
-
-                    // Inserting data into 'vendas' table
-                    String produtoVendido = "Capa de chuva";
-                    float valor = 5.00f;
-
-                    String sqlVenda = "INSERT INTO vendas (id_cliente, produto_vendido, valor) VALUES (?, ?, ?)";
-                    try (PreparedStatement stmtVenda = conexao.prepareStatement(sqlVenda)) {
-                        stmtVenda.setInt(1, idCliente);
-                        stmtVenda.setString(2, produtoVendido);
-                        stmtVenda.setFloat(3, valor);
-
-                        int linhasAfetadasVenda = stmtVenda.executeUpdate();
-
-                        if (linhasAfetadasVenda > 0) {
-                            System.out.println("Venda registrada com sucesso!");
-                        } else {
-                            System.out.println("Não foi possível registrar a venda.");
-                        }
-                    }
+            try (PreparedStatement stmtClientes = conexao.prepareStatement(sqlInserirClientes)) {
+                int linhasAfetadasClientes = stmtClientes.executeUpdate();
+                if (linhasAfetadasClientes > 0) {
+                    System.out.println("Dados de clientes inseridos com sucesso!");
                 } else {
-                    System.out.println("Não foi possível registrar o cliente.");
+                    System.out.println("Não foi possível inserir dados de clientes.");
                 }
             }
 
+            // Inserir dados na tabela vendas
+            String sqlInserirVendas = "INSERT INTO vendas (id_venda, id_cliente, produto_vendido, valor) VALUES " +
+                                      "(1, 1, 'Celular', 1200.00), " +
+                                      "(2, 2, 'Fones', 150.00)";
+
+            try (PreparedStatement stmtVendas = conexao.prepareStatement(sqlInserirVendas)) {
+                int linhasAfetadasVendas = stmtVendas.executeUpdate();
+                if (linhasAfetadasVendas > 0) {
+                    System.out.println("Dados de vendas inseridos com sucesso!");
+                } else {
+                    System.out.println("Não foi possível inserir dados de vendas.");
+                }
+            }
         } catch (SQLException e) {
             System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
         }
